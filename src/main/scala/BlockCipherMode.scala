5e031dacc16c14a3ce7d1068eb77698fd60776f9
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2015-01-06 23:25:35 +0100

    Added trait BlockCipherMode and CBC implementation.

---
+++ src/main/scala/BlockCipherMode.scala
@@ -0,0 +1,26 @@
+/* Copyright 2014 Richard Wiedenhoeft <richard@wiedenhoeft.xyz>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.wiedenhoeft.scalacrypt
+
+trait BlockCipherMode {
+
+  def preEncryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any])
+
+  def postEncryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any])
+
+  def preDecryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any])
+
+  def postDecryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any])
+}
---
+++ src/main/scala/mode.scala
@@ -0,0 +1,68 @@
+/* Copyright 2014 Richard Wiedenhoeft <richard@wiedenhoeft.xyz>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.wiedenhoeft.scalacrypt.mode
+
+trait CBC {
+
+  import scala.language.implicitConversions
+
+  // Define xor operator
+  private class ByteSeqWrapper(lhs: Seq[Byte]) {
+    def xor(rhs: Seq[Byte]): Seq[Byte] = for(i <- 0 until lhs.length) yield (lhs(i) ^ rhs(i)).toByte
+  }
+  private implicit def wrapByteSeq(seq: Seq[Byte]) = new ByteSeqWrapper(seq)
+
+  def iv: Seq[Byte]
+
+  def preEncryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any]) = {
+    val prev: Seq[Byte] = state match {
+      // Use the previous ciphertext block.
+      case Some(s) ⇒
+      s.asInstanceOf[Seq[Byte]]
+
+      // First block. Use the IV.
+      case _ ⇒
+      iv
+    }
+
+    // Xor the previous ciphertext block/IV to the cleartext block.
+    (block xor prev, None)
+  }
+
+  def postEncryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any]) = {
+    // Save the ciphertext for use in preEncryptBlock.
+    (block, Some(block))
+  }
+
+  def preDecryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any]) = {
+    state match {
+      // Supply previous ciphertext for xoring and save ciphertext.
+      case Some(s) ⇒
+      (block, Some((s.asInstanceOf[Seq[Byte]], block)))
+
+      // First Block. Supply IV for xoring and save ciphertext.
+      case _ ⇒
+      (block, Some((iv, block)))
+    }
+  }
+
+  def postDecryptBlock(block: Seq[Byte], state: Option[Any]): (Seq[Byte], Option[Any]) = {
+    // Tuple contains previous and current ciphertext block.
+    val tuple = state.get.asInstanceOf[(Seq[Byte],Seq[Byte])]
+    
+    // Xor decrypted block with previous ciphertext block. Set state to current ciphertext.
+    (block xor tuple._1, Some(tuple._2))
+  }
+}
