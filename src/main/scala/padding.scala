19717e49911eaaf8730b8acc5501508540436dff
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2015-01-07 09:02:24 +0100

    Added NoPadding trait.

--- src/main/scala/padding.scala
+++ src/main/scala/padding.scala
@@ -17,6 +17,32 @@ package xyz.wiedenhoeft.scalacrypt.padding
 import xyz.wiedenhoeft.scalacrypt._
 import scala.util.{ Try, Success, Failure }
 
+trait NoPadding extends BlockPadding {
+
+  def pad(input: Iterator[Seq[Byte]]): Iterator[Seq[Byte]] = new Iterator[Seq[Byte]] {
+
+    var buffer = Seq[Byte]()
+
+    def hasNext = input.hasNext || buffer.length > 0
+
+    def next = {
+      while(buffer.length < blockSize && input.hasNext) {
+        buffer = buffer ++ input.next
+      }
+      if(buffer.length < blockSize) {
+        //Most likely yields an error in the block cipher
+        buffer
+      } else {
+        val rv = buffer.slice(0, blockSize)
+        buffer = buffer.slice(blockSize, buffer.length)
+        rv
+      }
+    }
+  }
+
+  def unpad(input: Iterator[Seq[Byte]]): Iterator[Try[Seq[Byte]]] = input map { Success(_) }
+}
+
 trait PKCS7Padding extends BlockPadding {
 
   def pad(input: Iterator[Seq[Byte]]): Iterator[Seq[Byte]] = {
