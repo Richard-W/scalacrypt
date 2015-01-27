479e052523de9097c06007a921c52bb30a8f2b0c
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2015-01-27 18:13:49 +0100

    Fixed copyright notice.

--- src/main/scala/hash/JavaHash.scala
+++ src/main/scala/hash/JavaHash.scala
@@ -1,4 +1,4 @@
-/* Copyright 2014 Richard Wiedenhoeft <richard@wiedenhoeft.xyz>
+/* Copyright 2014, 2015 Richard Wiedenhöft <richard@wiedenhoeft.xyz>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
